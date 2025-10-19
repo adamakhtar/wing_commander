cd1d5c4af2a121ba13cc598b6b6c59dff14661af
Adam Akhtar <adamsubscribe@googlemail.com>
2025-10-19 20:38:47 +0100

    feat: add dev-minitest command for development testing
    - Add make dev-minitest command that builds dev version and launches against dummy minitest app
    - Update Makefile with simplified development workflow
    - Update dummy minitest Rakefile for better test setup
    
    Usage: make dev-minitest

--- dummy/minitest/test/test_helper.rb
+++ dummy/minitest/test/test_helper.rb
@@ -1,4 +1,8 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
 
 # Add lib directory to load path
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+# Configure JUnit XML reporter
+Minitest::Reporters.use! [Minitest::Reporters::JUnitReporter.new('test/reports')]
