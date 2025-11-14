f36938f92d7d67256e46d952dcf6f3e133bb8ea2
Adam Akhtar <adamsubscribe@googlemail.com>
2025-11-14 21:06:38 +0000

    refactor wing commander runner

--- dummy/minitest_example/bin/wing_commander_runner.rb
+++ dummy/minitest_example/bin/wing_commander_runner.rb
@@ -66,7 +66,7 @@ class WingCommanderRunner
   private
 
   class CommandBuilder
-    def initialize(test_file_patterns: ["test/**/*_test.rb", "test/**/test_*.rb"], lib_dirs: %w[lib test .])
+    def initialize(test_file_patterns:, lib_dirs:)
       @test_file_patterns = test_file_patterns
       @lib_dirs = lib_dirs
     end
@@ -97,62 +97,81 @@ class WingCommanderRunner
         raise "Cannot specify both test files and test cases"
       end
 
-      # Build command arguments
-      cmd_args = []
-      cmd_args << "-I#{@lib_dirs.join(File::PATH_SEPARATOR)}" unless @lib_dirs.empty?
-      cmd_args << "-w" # Enable warnings
+      cmd_args = build_base_cmd_args
 
       if test_files.any?
-        # Construct runner code: require minitest/autorun and all test files
-        requires = test_files.map { |f| %(require "#{f}") }
-        runner = ["require \"minitest/autorun\"", *requires].join("; ")
-
-        cmd_args << "-e" # flag to execute code
-        cmd_args << "'#{runner}'" # Single quotes like minitest/test_task.rb
-
-        shell_escaped_test_files = test_files.map{ "'#{_1}'" }
-        space_delimited_test_files = shell_escaped_test_files.join(' ')
-
-        return "ruby #{cmd_args.join(' ')} #{space_delimited_test_files}"
+        return build_test_files_command(cmd_args, test_files)
       elsif test_cases.any?
-        all_test_files = @test_file_patterns.flat_map { |pattern| Dir[pattern] }.select { |f| File.file?(f) }
-
-        if all_test_files.empty?
-          raise "No test files found matching patterns: #{@test_file_patterns.join(', ')}"
-        end
-
-        requires = all_test_files.map { |f| %(require "#{f}") }
-        runner = ["require \"minitest/autorun\"", *requires].join("; ")
-
-        cmd_args << "-e"
-        cmd_args << "'#{runner}'"
-        # TODO not sure why this -- is required. If removed we get an error. hashbangs
-        # between the test class name and case name may be problematic since we
-        # place these into a regex below for minitest. Perhpas this somehow corretly escapes them.
-        cmd_args << "--"
-        cmd_args << "-n"
-        pattern = "/#{test_cases.join("|")}/"
-        cmd_args << "'#{pattern}'"
-
-        return "ruby #{cmd_args.join(' ')}"
-      else # run all tests in the project using the test file globs
-        all_test_files = @test_file_patterns.flat_map { |pattern| Dir[pattern] }.select { |f| File.file?(f) }
-
-        if all_test_files.empty?
-          raise "No test files found matching patterns: #{@test_file_patterns.join(', ')}"
-        end
-
-        requires = all_test_files.map { |f| %(require "#{f}") }
-        runner = ["require \"minitest/autorun\"", *requires].join("; ")
-
-        cmd_args << "-e"
-        cmd_args << "'#{runner}'"
-
-        return "ruby #{cmd_args.join(' ')}"
+        return build_test_cases_command(cmd_args, test_cases)
+      else
+        return build_all_tests_command(cmd_args)
       end
     end
 
     private
+
+    def find_all_test_files
+      @test_file_patterns.flat_map { |pattern| Dir[pattern] }.select { |f| File.file?(f) }
+    end
+
+    def build_runner_code(test_files)
+      requires = test_files.map { |f| %(require "#{f}") }
+      ["require \"minitest/autorun\"", *requires].join("; ")
+    end
+
+    def build_base_cmd_args
+      cmd_args = []
+      cmd_args << "-I#{@lib_dirs.join(File::PATH_SEPARATOR)}" unless @lib_dirs.empty?
+      cmd_args << "-w" # Enable warnings
+      cmd_args
+    end
+
+    def find_all_test_files_with_validation
+      all_test_files = find_all_test_files
+      if all_test_files.empty?
+        raise "No test files found matching patterns: #{@test_file_patterns.join(', ')}"
+      end
+      all_test_files
+    end
+
+    def build_test_files_command(cmd_args, test_files)
+      runner = build_runner_code(test_files)
+
+      cmd_args << "-e" # flag to execute code
+      cmd_args << "'#{runner}'" # Single quotes like minitest/test_task.rb
+
+      shell_escaped_test_files = test_files.map{ "'#{_1}'" }
+      space_delimited_test_files = shell_escaped_test_files.join(' ')
+
+      "ruby #{cmd_args.join(' ')} #{space_delimited_test_files}"
+    end
+
+    def build_test_cases_command(cmd_args, test_cases)
+      all_test_files = find_all_test_files_with_validation
+      runner = build_runner_code(all_test_files)
+
+      cmd_args << "-e"
+      cmd_args << "'#{runner}'"
+      # TODO not sure why this -- is required. If removed we get an error. hashbangs
+      # between the test class name and case name may be problematic since we
+      # place these into a regex below for minitest. Perhpas this somehow corretly escapes them.
+      cmd_args << "--"
+      cmd_args << "-n"
+      pattern = "/#{test_cases.join("|")}/"
+      cmd_args << "'#{pattern}'"
+
+      "ruby #{cmd_args.join(' ')}"
+    end
+
+    def build_all_tests_command(cmd_args)
+      all_test_files = find_all_test_files_with_validation
+      runner = build_runner_code(all_test_files)
+
+      cmd_args << "-e"
+      cmd_args << "'#{runner}'"
+
+      "ruby #{cmd_args.join(' ')}"
+    end
 
     def valid_test_file?(file_path)
       return false unless File.file?(file_path)
